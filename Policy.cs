e4c88603fbcbe6eb6ffc300201819e9071f5d706
Gal Gur <gal.gurevich@gmail.com>
2022-09-01 14:30:36 +0300

    del commented code

--- Policy.cs
+++ Policy.cs
@@ -21,22 +21,6 @@ namespace TestRating
         {
             return 0;
         }
-        /*
-        #region Life Insurance
-        public bool IsSmoker { get; set; }
-        public decimal Amount { get; set; }
-        #endregion
-
-        #region Travel
-        public string Country { get; set; }
-        public int Days { get; set; }
-        #endregion
-
-        #region Health
-        public string Gender { get; set; }
-        public decimal Deductible { get; set; }
-        #endregion
-        */
     }
 
     public class TravelPolicy : Policy
